5e0ab5bea0ce64b037c7f54db446990954014506
Jan Mattar <jean.matar@campus.technion.ac.il>
2025-10-11 20:25:57 +0300

    finished 1.11
    made the statistics in a HTML Table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,9 +5,10 @@ const Button = ({onClick, text}) => (
 )
 
 const StatisticLine =({text, value}) => (
-  <div>
-    {text} {value}
-  </div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({good, neutral, bad}) => {
@@ -27,11 +28,14 @@ const Statistics = ({good, neutral, bad}) => {
   return(
      <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} /> 
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + "%"} /> 
+      <table>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} /> 
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + "%"} /> 
+      </table>
     </div>
    )
 
