35902c37c1e3aecb1bb78b40c9484458a302ac2e
Jan Mattar <jean.matar@campus.technion.ac.il>
2025-10-26 23:30:19 +0200

    added show button and connected it to openweahther api with live weather info

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,6 +2,13 @@ import { useState, useEffect} from 'react'
 import axios from 'axios'
 
 const CountryDetails = ({country}) => {
+  const apiKey = import.meta.env.VITE_WEATHER_KEY
+  const [weather, setWeather] = useState(null)
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}`)
+      .then(response => setWeather(response.data))
+  }, [country.capital]) 
   return(
     <div>
       <h1>{country.name.common}</h1>
@@ -16,11 +23,19 @@ const CountryDetails = ({country}) => {
         {Object.values(country.languages).map(lan => <li key={lan}>{lan}</li>)}
       </ul>
       <img src={country.flags.png} />
+      {weather && (
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <div>Temperature {(weather.main.temp - 273.15).toFixed(2)} Celsius</div>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+          <div>Wind {weather.wind.speed} m/s</div>
+        </div>
+      )}
     </div>
   )
 }
 
-const Results = ({filteredCountries}) => {
+const Results = ({filteredCountries, selectedCountry, setSelectedCountry}) => {
   const [countryData, setCountryData] = useState(null)
 
    useEffect(() => {
@@ -30,23 +45,36 @@ const Results = ({filteredCountries}) => {
         .then(response => {
           setCountryData(response.data)
         })
+    } else if(selectedCountry){
+      axios   
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${selectedCountry}`)
+        .then(response => {
+          setCountryData(response.data)
+      })
+    } else {
+      setCountryData(null)
     }
-  }, [filteredCountries])
+  }, [filteredCountries, selectedCountry])
 
   if(filteredCountries.length === 0){
     return null
   }else if(filteredCountries.length > 10){
     return <div>Too many matches, specify another filter</div>
   }else if(filteredCountries.length === 1){
-    return countryData? <CountryDetails country={countryData} /> : <p>loading...</p>
+    return countryData ? <CountryDetails country={countryData} /> : <p>loading...</p>
+  }else if(selectedCountry && countryData){
+    return <CountryDetails country={countryData} />
   }else{
-    return <div>{filteredCountries.map(country => <div key={country}>{country}</div>)}</div>
+    return (<div>
+      {filteredCountries.map(country => <div key={country}>
+        {country} <button onClick={() => setSelectedCountry(country)}>show</button></div>)}
+      </div>)
   } 
 }
 
 function App() {
-const apiKey = import.meta.env.VITE_WEATHER_KEY 
 const [newCountry, setNewCountry] = useState('')
+const [selectedCountry, setSelectedCountry] = useState(null)
 const [countries, setCountries] = useState([])
 const [filteredCountries, setfilteredCountries] = useState([])
 
@@ -61,12 +89,13 @@ useEffect(() => {
 const handleCountryChange = (event) => {
   setNewCountry(event.target.value)
   setfilteredCountries(countries.filter(country => country.toLowerCase().startsWith(event.target.value.toLowerCase())))
+  setSelectedCountry(null)
 }
 
   return (
     <div>
       find countries <input value={newCountry} onChange={handleCountryChange} />
-      <Results filteredCountries={filteredCountries} />
+      <Results filteredCountries={filteredCountries} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} />
     </div>
   )
 }
