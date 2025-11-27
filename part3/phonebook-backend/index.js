50b3b6065139eaac6ab9aac48b345e931728dc75
Jan Mattar <jean.matar@campus.technion.ac.il>
2025-11-27 12:17:08 +0200

    added cors and port changes dynamically for deployment

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
+
+app.use(cors());
 morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
 const customLogFormat = ':method :url :status :res[content-length] - :response-time ms :body'
 
@@ -65,7 +68,7 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people<br>${Date()}`);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
